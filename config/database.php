f6119dac17338286e2447dda888ff751248e5f0a
dimka1985 <dmitrytsibylskiy@gmail.com>
2018-08-05 19:08:22 +0300

    Configure queue

--- config/database.php
+++ config/database.php
@@ -143,6 +143,13 @@ return [
             'database' => 4,
         ],
 
+        'queue' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 5,
+        ],
+
     ],
 
 ];
