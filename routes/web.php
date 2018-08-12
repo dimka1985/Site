e92ae2a609146a85276e31dacf7c0b54ef00dc44
dimka1985 <dmitrytsibylskiy@gmail.com>
2018-08-12 16:41:37 +0300

    Activate Auth routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-//Auth::routes();
+Auth::routes();
 
-//Route::get('auth/{socialprovider}', 'Auth\AuthController@redirectToProvider');
-//Route::get('auth/{socialprovider}/callback', 'Auth\AuthController@handleProviderCallback');
+Route::get('auth/{socialprovider}', 'Auth\AuthController@redirectToProvider');
+Route::get('auth/{socialprovider}/callback', 'Auth\AuthController@handleProviderCallback');
 
 Route::get('/home', 'HomeController@index')->name('home');
