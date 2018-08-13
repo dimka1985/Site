24a37669adf0633d7de0de1f7d117c8a34f2f126
dimka1985 <dmitrytsibylskiy@gmail.com>
2018-08-13 17:43:04 +0300

    Fix register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,10 +5,10 @@
 @endsection
 
 @section('content')
-    <div class="container mt-5">
+    <div class="container my-5">
         <br>
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-xl-8">
                 <div class="card">
                     <div class="card-header text-center">{{ __('Register') }}</div>
 
@@ -18,10 +18,10 @@
                         {{ Form::open(['route' => 'register', 'aria-label' => __('Register'), 'files' => true]) }}
 
                         <div class="form-group row">
-                            {{ Form::label('name', __('Name') . '*', ['class' => 'col-md-4 col-form-label
-                            text-md-right']) }}
+                            {{ Form::label('name', __('Name') . '*', ['class' => 'col-lg-4 col-form-label
+                            text-lg-right']) }}
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <div class="input-group">
                                     {{ Form::text('name', old('name'), ['class' => 'form-control' .
                                     ((count($errors) > 0) ? ($errors->has('name') ? ' is-invalid' : ' is-valid') :
@@ -34,18 +34,18 @@
 
                                     @if ($errors->has('name'))
                                         <span class="invalid-feedback">
-                                                <strong>{{ $errors->first('name') }}</strong>
-                                            </span>
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            {{ Form::label('company', __('Company'), ['class' => 'col-md-4 col-form-label
-                            text-md-right']) }}
+                            {{ Form::label('company', __('Company'), ['class' => 'col-lg-4 col-form-label
+                            text-lg-right']) }}
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <div class="input-group">
                                     {{ Form::text('company', old('company'), ['class' => 'form-control' .
                                     ((count($errors) > 0) ? ($errors->has('company') ? ' is-invalid' :
@@ -58,20 +58,20 @@
 
                                     @if ($errors->has('company'))
                                         <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('company') }}</strong>
-                                            </span>
+                                            <strong>{{ $errors->first('company') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            {{ Form::label('phone', __('Phone') . ' +375', ['class' => 'col-md-4 col-form-label
-                            text-md-right']) }}
+                            {{ Form::label('phone', __('Phone') . ' +375', ['class' => 'col-lg-4 col-form-label
+                            text-lg-right']) }}
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <div class="row">
-                                    <div class="col-md-3">
+                                    <div class="col-lg-3">
                                         {{ Form::text('code', old('code'), ['class' => 'form-control' .
                                         ((count($errors) > 0) ? ($errors->has('code') ? ' is-invalid' :
                                         ' is-valid') : ''), 'placeholder' => __('Code'), 'minlength' =>
@@ -84,7 +84,7 @@
                                         @endif
                                     </div>
 
-                                    <div class="col-md-9">
+                                    <div class="col-lg-9">
                                         <div class="input-group">
                                             {{ Form::text('phone', old('phone'), ['class' => 'form-control' .
                                             ((count($errors) > 0) ? ($errors->has('phone') ? ' is-invalid' :
@@ -106,10 +106,10 @@
                         </div>
 
                         <div class="form-group row">
-                            {{ Form::label('email', __('E-Mail address') . '*', ['class' => 'col-md-4 col-form-label
-                            text-md-right']) }}
+                            {{ Form::label('email', __('E-Mail address') . '*', ['class' => 'col-lg-4 col-form-label
+                            text-lg-right']) }}
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <div class="input-group">
                                     {{ Form::email('email', old('email'), ['class' => 'form-control' .
                                     ((count($errors) > 0) ? ($errors->has('email') ? ' is-invalid' :
@@ -121,18 +121,18 @@
 
                                     @if ($errors->has('email'))
                                         <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('email') }}</strong>
-                                            </span>
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            {{ Form::label('password', __('Password') . '*', ['class' => 'col-md-4 col-form-label
-                            text-md-right']) }}
+                            {{ Form::label('password', __('Password') . '*', ['class' => 'col-lg-4 col-form-label
+                            text-lg-right']) }}
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <div class="input-group">
                                     {{ Form::password('password', ['class' => 'form-control' . ((count($errors) > 0) ? ($errors->has('password') ? ' is-invalid' : ' is-valid') : ''),
                                      'placeholder' => __('Enter your password'), 'minlength' => '6', 'maxlength' => '30', 'required']) }}
@@ -143,17 +143,18 @@
 
                                     @if ($errors->has('password'))
                                         <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            {{ Form::label('password_confirmation', __('Confirm password') . '*', ['class' => 'col-md-4 col-form-label text-md-right']) }}
+                            {{ Form::label('password_confirmation', __('Confirm password') . '*', ['class' =>
+                            'col-lg-4 col-form-label text-lg-right']) }}
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <div class="input-group">
                                     {{ Form::password('password_confirmation', ['class' => 'form-control' .
                                     ((count($errors) > 0) ? ($errors->has('password_confirmation') ?
@@ -166,18 +167,18 @@
 
                                     @if ($errors->has('password_confirmation'))
                                         <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                            </span>
+                                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            {{ Form::label('avatar', __('Avatar'), ['class' => 'col-md-4 col-form-label
-                            text-md-right']) }}
+                            {{ Form::label('avatar', __('Avatar'), ['class' => 'col-lg-4 col-form-label
+                            text-lg-right']) }}
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <div class="custom-file">
                                     {{ Form::file('avatar', ['class' => 'custom-file-input' .
                                     ((count($errors) > 0) ? ($errors->has('avatar') ? ' is-invalid' :
@@ -188,15 +189,15 @@
 
                                     @if ($errors->has('avatar'))
                                         <span class="invalid-feedback pt-2" role="alert">
-                                                <strong>{{ $errors->first('avatar') }}</strong>
-                                            </span>
+                                            <strong>{{ $errors->first('avatar') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-lg-6 offset-lg-4">
                                 <div class="checkbox">
                                     <div class="custom-control custom-checkbox">
                                         {{ Form::checkbox('terms', null, (old('terms') ? true : false),
@@ -209,8 +210,8 @@
 
                                         @if ($errors->has('terms'))
                                             <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $errors->first('terms') }}</strong>
-                                                </span>
+                                                <strong>{{ $errors->first('terms') }}</strong>
+                                            </span>
                                         @endif
                                     </div>
                                 </div>
@@ -218,7 +219,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-lg-6 offset-lg-4">
                                 {{ Form::submit(__('Register'), ['class' => 'btn btn-primary']) }}
                             </div>
                         </div>
@@ -226,7 +227,7 @@
                         <hr>
 
                         <div class="row">
-                            <div class="col-md-8 offset-md-2">
+                            <div class="col-lg-8 offset-lg-2">
                                 <h6 class="text-center">{{ __('Authorize with social networks') }}</h6>
                                 <hr>
                                 @foreach($socialproviders as $socialprovider)
