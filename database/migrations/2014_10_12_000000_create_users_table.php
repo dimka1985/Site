8461164ac9e605f03fcd0364e27139dde042472b
dimka1985 <dmitrytsibylskiy@gmail.com>
2018-08-13 14:51:10 +0300

    Rework User table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('company', 50)->nullable();
+            $table->string('phone', 12)->nullable();
             $table->string('email')->nullable();
             $table->string('password')->nullable();
             $table->unsignedInteger('socialprovider_id')->nullable();
             $table->foreign('socialprovider_id')->references('id')->on('socialproviders');
             $table->string('socialuser_id')->nullable();
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
