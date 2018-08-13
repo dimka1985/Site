a98cb9ab1deba26fea54f7b8d178b6f915dee4a6
dimka1985 <dmitrytsibylskiy@gmail.com>
2018-08-13 15:15:09 +0300

    Add basic lang ru files

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароли должны быть не менее 6 и не более 30 символов и соответствовать подтверждению.',
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Вам отправлена по электронной почте ссылка на сброс пароля!',
+    'token' => 'Этот токен восстановления пароля не является действительным.',
+    'user' => "Не удалось обнаружить пользователя с указанным e-mail адресом.",
+
+];
