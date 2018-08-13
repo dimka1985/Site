c0647b75c1ae72a5882378f2f56eaeb031a77fe2
dimka1985 <dmitrytsibylskiy@gmail.com>
2018-08-13 16:10:11 +0300

    Rework main view logic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -186,31 +186,9 @@
     </div>
 </nav>
 
-<div id="app">
-    @yield('content')
-</div>
+@yield('content')
 
 <hr class="mt-5">
-
-<script id="bx24_form_inline" data-skip-moving="true">
-  (function (w, d, u, b) {
-    w['Bitrix24FormObject'] = b
-    w[b] = w[b] || function () {
-      arguments[0].ref = u;
-      (w[b].forms = w[b].forms || []).push(arguments[0])
-    }
-    if (w[b]['forms']) {
-      return
-    }
-    let s = d.createElement('script')
-    s.async = 1
-    s.src = u + '?' + (1 * new Date())
-    let h = d.getElementsByTagName('script')[0]
-    h.parentNode.insertBefore(s, h)
-  })(window, document, 'https://ipnkuksar.bitrix24.by/bitrix/js/crm/form_loader.js', 'b24form')
-
-  b24form({'id': '4', 'lang': 'ru', 'sec': 'den8dk', 'type': 'inline'})
-</script>
 
 <footer id="contacts">
     <div class="jumbotron jumbotron-fluid">
