c64180955798573ec34dea3f154d7fd32fd8a625
dimka1985 <dmitrytsibylskiy@gmail.com>
2018-08-24 14:20:52 +0300

    Fix some logic

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,62 +1,62 @@
 @component('mail::message')
-    {{-- Greeting --}}
-    @if (! empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level == 'error')
-            # @lang('Whoops!')
-        @else
-            # @lang('Hello!')
-        @endif
-    @endif
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level == 'error')
+# @lang('Whoops!')
+@else
+# @lang('Hello!')
+@endif
+@endif
 
-    {{-- Intro Lines --}}
-    @foreach ($introLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+@endforeach
 
-    {{-- Action Button --}}
-    @isset($actionText)
-        <?php
-        switch ($level) {
-            case 'success':
-                $color = 'green';
-                break;
-            case 'error':
-                $color = 'red';
-                break;
-            default:
-                $color = 'blue';
-        }
-        ?>
-        @component('mail::button', ['url' => $actionUrl, 'color' => $color])
-            {{ $actionText }}
-        @endcomponent
-    @endisset
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+switch ($level) {
+case 'success':
+$color = 'green';
+break;
+case 'error':
+$color = 'red';
+break;
+default:
+$color = 'blue';
+}
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
 
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+@endforeach
 
-    {{-- Salutation --}}
-    @if (! empty($salutation))
-        {{ $salutation }}
-    @else
-        @lang('Regards'),<br>{{ config('app.name') }}
-    @endif
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('Regards'),<br>{{ config('app.name') }}
+@endif
 
-    {{-- Subcopy --}}
-    @isset($actionText)
-        @component('mail::subcopy')
-            @lang(
-                "Если у Вас есть проблемы с нажатием на кнопку \":actionText\", скопируйте и вставьте в браузер\n".
-                'ссылку: [:actionURL](:actionURL)',
-                [
-                    'actionText' => $actionText,
-                    'actionURL' => $actionUrl
-                ]
-            )
-        @endcomponent
-    @endisset
+{{-- Subcopy --}}
+@isset($actionText)
+@component('mail::subcopy')
+@lang(
+    "Если у Вас есть проблемы с нажатием на кнопку \":actionText\", скопируйте и вставьте в браузер\n".
+    'ссылку: [:actionURL](:actionURL)',
+    [
+        'actionText' => $actionText,
+        'actionURL' => $actionUrl
+    ]
+)
+@endcomponent
+@endisset
 @endcomponent
